6fa249e9222aa5fe08e6d05b07173dc0b893c178
Punit Arani <punitsai36@gmail.com>
2023-11-10 20:44:49 -0700

    Remove request batching from get_papers()

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from buff.openalex.models import WorkObject
 
 async def get_papers(entity_id: str, max_depth: int = 10, max_count: int = 100) -> list[WorkObject]:
     """
-    Get papers from the OpenAlex API in batches of 10, with limits on depth and count.
+    Get papers from the OpenAlex API without batching, with limits on depth and count.
 
     Args:
         entity_id (str): The ID of the entity for which to get papers.
@@ -35,38 +35,36 @@ async def get_papers(entity_id: str, max_depth: int = 10, max_count: int = 100) 
             pending_entities, next_entities = next_entities, []
             max_depth -= 1
 
-        while pending_entities and len(papers) < max_count:
-            # Adjust batch size based on remaining count
-            remaining_count = max_count - len(papers)
-            batch_size = min(len(pending_entities), 10, remaining_count)
-            batch = [pending_entities.pop() for _ in range(batch_size)]
+        # Prepare a list of unprocessed entity IDs
+        unprocessed_entities = [eid for eid in pending_entities if eid not in processed_entities]
 
-            # Perform batch requests
-            results = await asyncio.gather(
-                *(
-                    Work(eid).get()
-                    for eid in batch
-                    if eid not in processed_entities
-                ),
-                return_exceptions=False
-            )
+        # Perform requests for all unprocessed entities
+        results = await asyncio.gather(
+            *(Work(eid).get() for eid in unprocessed_entities),
+            return_exceptions=False
+        )
 
-            for result in results:
-                if isinstance(result, Exception):
-                    print(f"Error during batch request: {result}")
-                else:
-                    papers.append(result)
-                    # Process results to get next entities
-                    for url in result.referenced_works:
-                        url = str(url)
-                        if url.startswith("https://openalex.org/"):
-                            next_entity_id = url[21:]
-                            if next_entity_id not in processed_entities:
-                                next_entities.append(next_entity_id)
+        # Reset pending_entities for the next iteration
+        pending_entities = []
 
-                # Early exit if max_count is reached
-                if len(papers) >= max_count:
-                    return papers
+        for result in results:
+            if isinstance(result, Exception):
+                print(f"Error during request: {result}")
+            else:
+                papers.append(result)
+                processed_entities.add(result.id)
+
+                # Process results to get next entities
+                for url in result.referenced_works:
+                    url = str(url)
+                    if url.startswith("https://openalex.org/"):
+                        next_entity_id = url[21:]
+                        if next_entity_id not in processed_entities:
+                            next_entities.append(next_entity_id)
+
+            # Early exit if max_count is reached
+            if len(papers) >= max_count:
+                return papers
 
     return papers
 
@@ -84,7 +82,7 @@ async def main():
         papers_df = pd.DataFrame(
             [
                 {
-                    "id": work.id,
+                    "id": str(work.id),
                     "title": work.title,
                 }
                 for work in works
@@ -97,12 +95,17 @@ async def main():
 
 
 if __name__ == "__main__":
-    # Streamlit config
+    import platform
+
     st.set_page_config(
         page_title="AutoResearcher",
         page_icon="🔍",
         layout="wide",
         initial_sidebar_state="expanded",
     )
+
+    # Check if windows
+    if platform.system() == 'Windows':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
     asyncio.run(main())
