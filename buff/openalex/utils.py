d22f6a83a49144d8f412cab52ab2955bf0fcdf83
Punit Arani <punitsai36@gmail.com>
2023-12-23 20:38:37 -0700

    Feature: add doi_to_entity_id() to help convert DOI to OpenAlex Entity ID

--- buff/openalex/utils.py
+++ buff/openalex/utils.py
@@ -1,5 +1,6 @@
 """buff.openalex.utils.py"""
 
+import requests
 from pydantic import HttpUrl
 
 from .errors import OpenAlexError
@@ -22,3 +23,29 @@ def parse_id_from_url(url: str | HttpUrl) -> str:
         if url[0] in ["W", "A", "I", "S"]:
             return url
     raise OpenAlexError("Invalid OpenAlex URL")
+
+
+def doi_to_entity_id(doi: str) -> str:
+    """
+    Convert a DOI to an OpenAlex entity ID
+
+    Args:
+        doi (str): DOI to convert to an OpenAlex entity ID
+
+    Returns:
+        str: OpenAlex entity ID
+    """
+
+    if not doi.startswith("https://doi.org/"):
+        doi = f"https://doi.org/{doi}"
+
+    url = f"https://api.openalex.org/works/doi/{doi}"
+    response = requests.get(url)
+    response.raise_for_status()
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        return parse_id_from_url(data["id"])
+    else:
+        raise OpenAlexError(f"Invalid DOI: {doi}")
