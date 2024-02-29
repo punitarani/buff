f3797b491704db77e9c68079749dace1dcd4f0a5
Punit Arani <punitsai36@gmail.com>
2024-02-29 14:39:14 -0700

    Build: remove chromadb and supabase

--- buff/secrets.py
+++ buff/secrets.py
@@ -8,7 +8,8 @@ from pydantic import BaseModel
 class Secrets(BaseModel):
     """Secrets loaded from environment variables"""
 
-    OPENAI_API_KEY: str
+    # Cohere
+    COHERE_API_KEY: str
 
     # Mongo
     MONGO_HOST: str
@@ -16,11 +17,21 @@ class Secrets(BaseModel):
     MONGO_USERNAME: str
     MONGO_PASSWORD: str
 
+    # OpenAI
+    OPENAI_API_KEY: str
+
+    # Pinecone
+    PINECONE_API_KEY: str
+
+    # Together AI
+    TOGETHER_API_KEY: str
+
     @classmethod
     def load(cls) -> "Secrets":
         """Load secrets from environment variables"""
-        OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-        assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable not set"
+
+        # Cohere
+        COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 
         # Mongo
         MONGO_HOST = os.getenv("MONGO_HOST")
@@ -32,10 +43,25 @@ class Secrets(BaseModel):
         assert MONGO_USERNAME, "MONGO_USERNAME environment variable not set"
         assert MONGO_PASSWORD, "MONGO_PASSWORD environment variable not set"
 
+        # OpenAI
+        OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+        assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable not set"
+
+        # Pinecone
+        PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+        assert PINECONE_API_KEY, "PINECONE_API_KEY environment variable not set"
+
+        # Together AI
+        TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
+        assert TOGETHER_API_KEY, "TOGETHER_API_KEY environment variable not set"
+
         return cls(
-            OPENAI_API_KEY=OPENAI_API_KEY,
+            COHERE_API_KEY=COHERE_API_KEY,
             MONGO_HOST=MONGO_HOST,
             MONGO_DB=MONGO_DB,
             MONGO_USERNAME=MONGO_USERNAME,
             MONGO_PASSWORD=MONGO_PASSWORD,
+            OPENAI_API_KEY=OPENAI_API_KEY,
+            PINECONE_API_KEY=PINECONE_API_KEY,
+            TOGETHER_API_KEY=TOGETHER_API_KEY,
         )
