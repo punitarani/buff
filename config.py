23c45f4960058a39c1e5ec91bc7df48ae043029e
Punit Arani <punitsai36@gmail.com>
2024-02-29 13:42:17 -0700

    Feature: add LOGS_DIR

--- config.py
+++ config.py
@@ -8,4 +8,9 @@ DATA_DIR = PROJECT_PATH.joinpath("data")
 if not DATA_DIR.exists():
     DATA_DIR.mkdir()
 
+LOGS_DIR = PROJECT_PATH.joinpath("logs")
+if not LOGS_DIR.exists():
+    LOGS_DIR.mkdir()
+
+
 EMAIL = "email@gmail.com"
