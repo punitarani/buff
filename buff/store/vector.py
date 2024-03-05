ec12ada4a39d94566e3bd33367e61c5797495d72
Punit Arani <punitsai36@gmail.com>
2024-03-04 18:49:17 -0700

    Feature: add papers index to pinecone

--- buff/store/vector.py
+++ buff/store/vector.py
@@ -8,3 +8,7 @@ PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 assert PINECONE_API_KEY, "Env var PINECONE_API_KEY not set"
 
 pc = Pinecone(api_key=PINECONE_API_KEY)
+
+
+# papers index
+papers_index = pc.Index(name="papers")
