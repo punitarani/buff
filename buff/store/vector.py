93ad317f1a726701812b1b56a918a7fa3f4f8192
Punit Arani <punitsai36@gmail.com>
2024-03-04 18:48:58 -0700

    Feature: initialize pinecone client

---
+++ buff/store/vector.py
@@ -0,0 +1,10 @@
+"""buff/store/vector.py"""
+
+import os
+
+from pinecone import Pinecone
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+assert PINECONE_API_KEY, "Env var PINECONE_API_KEY not set"
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
