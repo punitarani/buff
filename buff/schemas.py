a9a23d7d09d3e475a92805d45d0926114b0cb013
Punit Arani <punitsai36@gmail.com>
2024-03-04 19:25:06 -0700

    Feature: add Work and WorkChunk schemas

--- buff/schemas.py
+++ buff/schemas.py
@@ -4,6 +4,7 @@ buff/schemas.py
 Database Schemas
 """
 
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, Text
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -11,3 +12,22 @@ class Base(DeclarativeBase):
     """Base class for sql database models"""
 
     pass
+
+
+class Work(Base):
+    __tablename__ = "works"
+
+    id = Column(Text, primary_key=True, index=True)
+    title = Column(Text, nullable=False)
+    doi = Column(Text, unique=True, nullable=True)
+    pdf_url = Column(Text, nullable=True)
+    embed = Column(Boolean, default=False, nullable=False)
+
+
+class WorkChunk(Base):
+    __tablename__ = "work_chunks"
+
+    id = Column(Text, primary_key=True, index=True)
+    work_id = Column(Text, ForeignKey(Work.id, ondelete="CASCADE"), nullable=False)
+    index = Column(Integer, nullable=False)
+    text = Column(Text, nullable=False)
