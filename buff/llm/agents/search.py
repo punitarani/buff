0c94233b22d35ceea2fe1413775ed934fc158a8a
Punit Arani <punitsai36@gmail.com>
2024-03-06 01:58:35 -0700

    Feature: create search_web agent

--- buff/llm/agents/search.py
+++ buff/llm/agents/search.py
@@ -2,10 +2,32 @@
 
 from buff.llm.client import cohere
 
+
+SEARCH_WEB_PREAMBLE = """
+You are a scientist with access to the internet.
+Your task is to answer the following question concisely and accurately using web-search.
+""".strip
+
 SEARCH_WIKI_PREAMBLE = """
 You are a scientist with access to Wikipedia.
 Your task is to answer the following question concisely and accurately using information from Wikipedia.
 """.strip
+
+
+async def search_web(question: str) -> str:
+    """Search the internet for the answer to a question."""
+
+    prediction = await cohere.chat(
+        message=question,
+        preamble_override=SEARCH_WEB_PREAMBLE,
+        model="command",
+        temperature=0,
+        stream=False,
+        citation_quality="accurate",
+        connectors=[{"id": "web-search"}],
+        documents=[],
+    )
+    return prediction.text
 
 
 async def search_wiki(question: str) -> str:
