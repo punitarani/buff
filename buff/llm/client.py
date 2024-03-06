a6ce921119dea8986bc2c46c147b70e4cae9131f
Punit Arani <punitsai36@gmail.com>
2024-03-06 01:48:39 -0700

    Feature: initialize cohere client

--- buff/llm/client.py
+++ buff/llm/client.py
@@ -1,7 +1,10 @@
 """buff/llm/client.py"""
 
+import cohere
 from openai import AsyncOpenAI
 
 from buff import SECRETS
+
+cohere = cohere.AsyncClient(api_key=SECRETS.COHERE_API_KEY)
 
 openai = AsyncOpenAI(api_key=SECRETS.OPENAI_API_KEY)
