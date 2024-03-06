80a20b25a24d7d03db15d52e55d0824f7595ce01
Punit Arani <punitsai36@gmail.com>
2024-03-05 22:53:11 -0700

    Feature: store citations and references in SQL database

--- buff/openalex/work.py
+++ buff/openalex/work.py
@@ -5,6 +5,8 @@ from json import JSONDecodeError
 import httpx
 from aiolimiter import AsyncLimiter
 from pydantic import ValidationError
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import Session, sessionmaker
 from tenacity import (
     retry,
     retry_if_exception_type,
@@ -12,7 +14,9 @@ from tenacity import (
     wait_exponential,
 )
 
+from buff import schemas
 from buff.store.mongo import mongo_client
+from buff.store.sql import get_engine
 from config import EMAIL
 
 from .errors import InvalidEntityID, OpenAlexError
@@ -34,8 +38,6 @@ class Work:
 
     mongo_db_openalex = mongo_client["openalex"]
     mongo_collection_works = mongo_db_openalex["works"]
-    mongo_collection_citations = mongo_db_openalex["citations"]
-    mongo_collection_references = mongo_db_openalex["references"]
 
     def __init__(self, entity_id: str) -> None:
         """
@@ -57,6 +59,9 @@ class Work:
         self.idx: str = f"{self.BASE_URL}{self.entity_id}"
 
         self._data: WorkObject | None = None
+
+        self.sql_engine: Engine = get_engine()
+        self.sql_session: sessionmaker[Session] = sessionmaker(engine=self.sql_engine)
 
     @property
     async def data(self) -> WorkObject:
@@ -121,6 +126,18 @@ class Work:
             upsert=True,
         )
 
+        # Save the WorkObject data to the SQL database
+        work = schemas.Work(
+            id=self._data.id,
+            title=self._data.title,
+            doi=self._data.doi,
+            pdf_url=self._data.best_oa_location.pdf_url,
+            embed=False,
+        )
+        with self.sql_session() as session:
+            session.add(work)
+            session.commit()
+
         return self._data
 
     async def citations(
@@ -142,36 +159,41 @@ class Work:
 
         TODO: add support for batched requests
         """
-        # Try to get the citations from MongoDB
-        db_citations = await self.mongo_collection_citations.find_one({"id": self.idx})
-        if db_citations:
-            citation_ids = db_citations["citations"][:limit]
-
-            # Fetch all citation works in a single query
-            citation_works_cursor = self.mongo_collection_works.find(
-                {"id": {"$in": citation_ids}}
+        # Try to get the citations from the database
+        with self.sql_session() as session:
+            work_citations = (
+                session.query(schemas.WorkCitations)
+                .filter(schemas.WorkCitations.work_id == self._data.id)
+                .first()
             )
+            # TODO: check that the citations count match and/or are at limit
+            if work_citations:
+                citation_ids = work_citations.citations[:limit]
 
-            # Convert cursor to list
-            citation_works_list = await citation_works_cursor.to_list(length=None)
+                # Fetch all citation works in a single query
+                citation_works_cursor = self.mongo_collection_works.find(
+                    {"id": {"$in": citation_ids}}
+                )
 
-            # Sort the list of works by 'cited_by_count' in descending order
-            sorted_works = sorted(
-                citation_works_list, key=lambda x: x["cited_by_count"], reverse=True
-            )
+                # Convert cursor to list
+                citation_works_list = await citation_works_cursor.to_list(length=None)
 
-            # Extract the sorted IDs
-            sorted_ids = [work["id"] for work in sorted_works]
+                # Sort the list of works by 'cited_by_count' in descending order
+                sorted_works = sorted(
+                    citation_works_list, key=lambda x: x["cited_by_count"], reverse=True
+                )
 
-            # Limit the number of IDs
-            citation_ids = sorted_ids[:limit]
+                # Extract the sorted IDs
+                sorted_ids = [work["id"] for work in sorted_works]
 
-            # TODO: what happens if the cursor is missing some works?
-            citation_works = {
-                work["id"]: WorkObject(**work) for work in sorted_works[:limit]
-            }
+                # Limit the number of IDs
+                citation_ids = sorted_ids[:limit]
 
-            return citation_ids, citation_works
+                citation_works = {
+                    work["id"]: WorkObject(**work) for work in sorted_works[:limit]
+                }
+
+                return citation_ids, citation_works
 
         per_page: int = 200  # OpenAlex supports 1-200 per page
         limit = min(limit, 10000)  # Ensure limit is under 10,000 (OpenAlex API limit)
@@ -183,8 +205,10 @@ class Work:
         page: int = 1
         max_pages: int = 1
 
+        citation_count = None
         citation_ids = []
         citation_works = {}
+        citation_works_schemas = {}
 
         while len(citations) < limit:
             try:
@@ -192,6 +216,9 @@ class Work:
             except Exception as e:
                 print(f"Error fetching data: {e}")
                 break
+
+            if citation_count is None:
+                citation_count = data["meta"]["count"]
 
             new_citations = data["results"]
             if not new_citations:
@@ -203,7 +230,18 @@ class Work:
                     citation_id = citation.get("id")
                     if citation_id:
                         citation_ids.append(citation_id)
-                        citation_works[citation_id] = WorkObject(**citation)
+
+                        citation_work = WorkObject(**citation)
+                        citation_works[citation_id] = citation_work
+
+                        citation_work_schema = schemas.Work(
+                            id=citation_id,
+                            title=citation_work.title,
+                            doi=citation_work.doi,
+                            pdf_url=citation_work.best_oa_location.pdf_url,
+                            embed=False,
+                        )
+                        citation_works_schemas[citation_id] = citation_work_schema
                     # TODO: handle citations without IDs (shouldn't happen)
                 except OpenAlexError or ValidationError:
                     pass
@@ -219,12 +257,16 @@ class Work:
                 break
             page += 1
 
-        # Save the Citation IDs to MongoDB
-        await self.mongo_collection_citations.update_one(
-            filter={"id": self.idx},
-            update={"$set": {"citations": citation_ids}},
-            upsert=True,
+        # Save the Citation IDs to the SQL database
+        work_citations = schemas.WorkCitations(
+            id=self.idx,
+            work_id=self._data.id,
+            citations=citation_ids,
+            count=citation_count,
         )
+        with self.sql_session() as session:
+            session.add(work_citations)
+            session.commit()
 
         if save_all:
             # Save the Citation Works to MongoDB
@@ -235,9 +277,16 @@ class Work:
                     upsert=True,
                 )
 
+            # Save the Citation Works to the SQL database
+            with self.sql_session() as session:
+                for citation_work_schema in citation_works_schemas.values():
+                    session.merge(citation_work_schema)
+                session.commit()
+
             return citation_ids, citation_works
         else:
             return_citations = {}
+
             # Save only the Works within the limit to MongoDB
             for cit_id in citation_ids[:limit]:
                 citation_work = citation_works[cit_id]
@@ -247,6 +296,13 @@ class Work:
                     update={"$set": citation_work.model_dump(mode="json")},
                     upsert=True,
                 )
+
+            # Save only the Works within the limit to the SQL database
+            with self.sql_session() as session:
+                for citation_work_schema in citation_works_schemas.values():
+                    session.merge(citation_work_schema)
+                session.commit()
+
             return citation_ids[:limit], return_citations
 
     async def references(
@@ -259,47 +315,50 @@ class Work:
         Args:
             limit (int): Maximum number of references to fetch.
                 Default: 1000. Maximum: 10,000.
-            save_all (bool): Whether to save all the citations beyond the limit.
+            save_all (bool): Whether to save all the references beyond the limit.
 
         Returns:
             tuple[list[str], dict[str, WorkObject]]:
                 - List of IDs URLs of the references
                 - Dictionary of {id: WorkObject} of all the referenced works
         """
-        # Try to get the references from MongoDB
-        db_references = await self.mongo_collection_references.find_one(
-            {"id": self.idx}
-        )
-
-        if db_references:
-            # Get the reference IDs from MongoDB
-            reference_ids = db_references["references"]
-
-            # Fetch all reference works in a single query
-            reference_works_cursor = self.mongo_collection_works.find(
-                {"id": {"$in": reference_ids}}
+        # Try to get the references from the database
+        with self.sql_session() as session:
+            work_references = (
+                session.query(schemas.WorkReferences)
+                .filter(schemas.WorkReferences.work_id == self._data.id)
+                .first()
             )
+            # TODO: check that the references count match and/or are at limit
+            if work_references:
+                reference_ids = work_references.references[:limit]
 
-            # Convert cursor to list
-            reference_works_list = await reference_works_cursor.to_list(length=None)
+                # Fetch all reference works in a single query
+                reference_works_cursor = self.mongo_collection_works.find(
+                    {"id": {"$in": reference_ids}}
+                )
 
-            # Sort the list of works by 'cited_by_count' in descending order
-            sorted_works = sorted(
-                reference_works_list, key=lambda x: x["cited_by_count"], reverse=True
-            )
+                # Convert cursor to list
+                reference_works_list = await reference_works_cursor.to_list(length=None)
 
-            # Extract the sorted IDs
-            sorted_ids = [work["id"] for work in sorted_works]
+                # Sort the list of works by 'cited_by_count' in descending order
+                sorted_works = sorted(
+                    reference_works_list,
+                    key=lambda x: x["cited_by_count"],
+                    reverse=True,
+                )
 
-            # Limit the number of IDs
-            reference_ids = sorted_ids[:limit]
+                # Extract the sorted IDs
+                sorted_ids = [work["id"] for work in sorted_works]
 
-            # TODO: what happens if the cursor is missing some works?
-            reference_works = {
-                work["id"]: WorkObject(**work) for work in sorted_works[:limit]
-            }
+                # Limit the number of IDs
+                reference_ids = sorted_ids[:limit]
 
-            return reference_ids, reference_works
+                reference_works = {
+                    work["id"]: WorkObject(**work) for work in sorted_works[:limit]
+                }
+
+                return reference_ids, reference_works
 
         per_page: int = 200  # OpenAlex supports 1-200 per page
         limit = min(limit, 10000)  # Ensure limit is under 10,000 (OpenAlex API limit)
@@ -311,8 +370,10 @@ class Work:
         page: int = 1
         max_pages: int = 1
 
+        reference_count = None
         reference_ids = []
         reference_works = {}
+        reference_works_schemas = {}
 
         while len(references) < limit:
             try:
@@ -320,6 +381,9 @@ class Work:
             except Exception as e:
                 print(f"Error fetching data: {e}")
                 break
+
+            if reference_count is None:
+                reference_count = data["meta"]["count"]
 
             new_references = data["results"]
             if not new_references:
@@ -331,9 +395,20 @@ class Work:
                     reference_id = reference.get("id")
                     if reference_id:
                         reference_ids.append(reference_id)
-                        reference_works[reference_id] = WorkObject(**reference)
+
+                        reference_work = WorkObject(**reference)
+                        reference_works[reference_id] = reference_work
+
+                        reference_work_schema = schemas.Work(
+                            id=reference_id,
+                            title=reference_work.title,
+                            doi=reference_work.doi,
+                            pdf_url=reference_work.best_oa_location.pdf_url,
+                            embed=False,
+                        )
+                        reference_works_schemas[reference_id] = reference_work_schema
                     # TODO: handle references without IDs (shouldn't happen)
-                except ValidationError:
+                except OpenAlexError or ValidationError:
                     pass
 
             if total_references is None:
@@ -347,12 +422,16 @@ class Work:
                 break
             page += 1
 
-        # Save the Reference IDs to MongoDB
-        await self.mongo_collection_references.update_one(
-            filter={"id": self.idx},
-            update={"$set": {"references": reference_ids}},
-            upsert=True,
+        # Save the Reference IDs to the SQL database
+        work_references = schemas.WorkReferences(
+            id=self.idx,
+            work_id=self._data.id,
+            references=reference_ids,
+            count=reference_count,
         )
+        with self.sql_session() as session:
+            session.add(work_references)
+            session.commit()
 
         if save_all:
             # Save the Reference Works to MongoDB
@@ -363,19 +442,30 @@ class Work:
                     upsert=True,
                 )
 
+            # Save the Reference Works to the SQL database
+            with self.sql_session() as session:
+                for reference_work_schema in reference_works_schemas.values():
+                    session.merge(reference_work_schema)
+                session.commit()
+
             return reference_ids, reference_works
         else:
             return_references = {}
+
             # Save only the Works within the limit to MongoDB
             for ref_id in reference_ids[:limit]:
-                try:
-                    reference_work = reference_works[ref_id]
-                    return_references[ref_id] = reference_work
-                    await self.mongo_collection_works.update_one(
-                        filter={"id": str(reference_work.id)},
-                        update={"$set": reference_work.model_dump(mode="json")},
-                        upsert=True,
-                    )
-                except KeyError:
-                    pass
+                reference_work = reference_works[ref_id]
+                return_references[ref_id] = reference_work
+                await self.mongo_collection_works.update_one(
+                    filter={"id": str(reference_work.id)},
+                    update={"$set": reference_work.model_dump(mode="json")},
+                    upsert=True,
+                )
+
+            # Save only the Works within the limit to the SQL database
+            with self.sql_session() as session:
+                for reference_work_schema in reference_works_schemas.values():
+                    session.merge(reference_work_schema)
+                session.commit()
+
             return reference_ids[:limit], return_references
