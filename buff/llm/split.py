7a35821dc3d0fb37170efc8e4a99e097d03ac721
Punit Arani <punitsai36@gmail.com>
2024-03-06 19:13:03 -0700

    Feature: add split_text() function

---
+++ buff/llm/split.py
@@ -0,0 +1,17 @@
+"""buff/llm/split.py"""
+
+from unstructured.partition.auto import partition_text
+
+
+def split_text(text: str) -> list[str]:
+    """
+    Split the text into chunks.
+
+    Args:
+        text (str): The text to chunk
+
+    Returns:
+        list[str]: List of chunks
+    """
+    elements = partition_text(text=text, min_partition=40, max_partition=4000)
+    return [chunk.text for chunk in elements]
