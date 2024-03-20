0b4845b2f1f126a393896cb5f1f4264c5359d583
Punit Arani <punitsai36@gmail.com>
2024-03-19 19:48:26 -0700

    Feature: create embed_papers.py script

---
+++ scripts/embed_papers.py
@@ -0,0 +1,78 @@
+"""scripts/embed_papers.py"""
+
+import json
+
+from buff.llm.embed import embed_texts
+from buff.llm.models import Document, DocumentMetadata
+from buff.llm.split import split_text
+from buff.openalex import Work
+from buff.store.vector import pc_papers
+from buff.utils import sanitize_name
+from config import DATA_DIR, PAPERS_DIR
+
+PAPERS_TXT_DIR = PAPERS_DIR.joinpath("txt")
+NETWORK_FP = DATA_DIR.joinpath("network.json")
+WORKS_FP = DATA_DIR.joinpath("works.json")
+
+with open(NETWORK_FP, "r", encoding="utf-8") as f:
+    WORKS = json.load(f).get("nodes", [])
+
+
+async def process_paper(work_id: str) -> None:
+    """Process a paper"""
+    work = await Work(work_id).data
+    doi = str(work.doi)
+    filename = sanitize_name(doi) + ".txt"
+    txt_fp = PAPERS_TXT_DIR.joinpath(filename)
+
+    if not txt_fp.exists():
+        print(f"TXT not found: {work_id} - {doi}")
+        return
+
+    with open(txt_fp, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    # Split the text
+    chunks = split_text(text)
+
+    # Embed the chunks
+    embeds = await embed_texts(texts=chunks, input_type="search_document")
+
+    # Build the documents
+    documents = [
+        Document(
+            id=f"{work_id}#{i}",
+            values=embed,
+            metadata=DocumentMetadata(
+                index=i,
+                work_id=work_id,
+                doi=doi,
+                text=chunk
+            ),
+        )
+        for i, (chunk, embed) in enumerate(zip(chunks, embeds))
+    ]
+
+    # Convert the documents to JSON
+    docs = [doc.model_dump(mode="json") for doc in documents]
+
+    # Upsert the documents into the vector database
+    pc_papers.upsert(docs, namespace="papers")
+
+
+async def main():
+    """main function"""
+    semaphore = asyncio.Semaphore(10)  # Allow up to 10 concurrent tasks
+
+    async def process_paper_with_semaphore(work_id: str) -> None:
+        async with semaphore:
+            await process_paper(work_id)
+
+    tasks = [process_paper_with_semaphore(work["id"]) for work in WORKS]
+    await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
