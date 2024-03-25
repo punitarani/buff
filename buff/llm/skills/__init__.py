5dd6ffe38ba5e76cfaf2d066002f792c26d1d77e
Punit Arani <punitsai36@gmail.com>
2024-03-25 16:13:44 -0700

    Feature: add summarize_papers()

--- buff/llm/skills/__init__.py
+++ buff/llm/skills/__init__.py
@@ -2,7 +2,7 @@
 
 from .analyze import analyze_texts
 from .search import search_memory, search_web, search_wiki
-from .summarize import summarize_paper
+from .summarize import summarize_papers
 from .writer import write
 
 __all__ = [
@@ -10,6 +10,6 @@ __all__ = [
     "search_memory",
     "search_web",
     "search_wiki",
-    "summarize_paper",
+    "summarize_papers",
     "write",
 ]
--- buff/llm/skills/summarize.py
+++ buff/llm/skills/summarize.py
@@ -1,9 +1,20 @@
 """buff/llm/skills/summarize.py"""
 
+import asyncio
+from collections import Counter
+
+from langchain.chains import AnalyzeDocumentChain
 from langchain.chains.summarize import load_summarize_chain
-from langchain.schema import Document
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import ChatOpenAI
 from tenacity import retry, stop_after_attempt, wait_fixed
+
+from buff.llm.client import cohere
+from buff.llm.embed import embed_texts
+from buff.openalex.download import get_paper_text
+from buff.openalex.work import Work, WorkObject
+from buff.store.vector import pc_papers
 
 SUMMARIZE_PAPER_SYSTEM_PROMPT = """
 You will be provided the contents of a research paper.
@@ -16,8 +27,96 @@ Your task is to summarize the meeting notes as follows:
 - Write a list of potential next steps or research directions.
 """.strip()
 
+question_prompt_template = """
+Please provide a summary of the following text.
+Write a concise summary of the following:
+TEXT: {text}
+SUMMARY:
+""".strip()
+question_prompt = PromptTemplate(
+    template=question_prompt_template, input_variables=["text"]
+)
+
+refine_prompt_template = """
+Write a concise summary of the following text delimited by triple backquotes.
+Return your response in bullet points which covers the key points of the text.
+```{text}```
+BULLET POINT SUMMARY:
+""".strip()
+refine_prompt = PromptTemplate(
+    template=refine_prompt_template, input_variables=["text"]
+)
+
 LLM_OPENAI = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-0125")
-SUMMARIZE_CHAIN = load_summarize_chain(llm=LLM_OPENAI, chain_type="stuff")
+SUMMARIZE_CHAIN = AnalyzeDocumentChain(
+    combine_docs_chain=load_summarize_chain(
+        llm=LLM_OPENAI,
+        chain_type="refine",
+        question_prompt=question_prompt,
+        refine_prompt=refine_prompt,
+        return_intermediate_steps=False,
+    ),
+    text_splitter=RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=12288, chunk_overlap=1024
+    ),
+)
+
+
+async def summarize_papers(topic: str, n: int = 5) -> str:
+    """
+    Summarize the texts on the given topic.
+
+    Args:
+        topic (str): Topic of the texts
+        n (int): Maximum number of texts to summarize
+
+    Returns:
+        str: Summary of the texts
+    """
+
+    chat_response = await cohere.chat(
+        message=topic, model="command-light", search_queries_only=True
+    )
+    search_queries = [query["text"] for query in chat_response.search_queries]
+    print(search_queries)
+
+    q_vectors = await embed_texts(texts=search_queries, input_type="search_query")
+
+    work_ids = []
+    for q_vector in q_vectors:
+        docs = pc_papers.query(
+            vector=q_vector,
+            top_k=5,
+            include_values=False,
+            include_metadata=False,
+            namespace="papers",
+        )
+        matches = docs.get("matches", [])
+
+        # Iterate through the matches' id's
+        # id format: work_id#idx
+        work_ids.extend([match["id"].split("#")[0] for match in matches])
+    print(work_ids)
+
+    # Get the n most common work_id's
+    work_ids = [work_id for work_id, _ in Counter(work_ids).most_common(n)]
+
+    works: list[WorkObject] = [
+        await Work(entity_id=work_id).get() for work_id in work_ids
+    ]
+    print([work.title for work in works])
+
+    texts: dict[str, str] = {work.id: await get_paper_text(work=work) for work in works}
+
+    # Asynchronously summarize the texts
+    summaries = await asyncio.gather(
+        *(summarize_text(text=text) for work_id, text in texts.items())
+    )
+    print(summaries)
+
+    return "\n\n".join(
+        f"{work_id}\n{summary}" for work_id, summary in zip(texts.keys(), summaries)
+    )
 
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
@@ -32,4 +131,4 @@ async def summarize_text(text: str) -> str:
         str: Summary of the paper
     """
 
-    return await SUMMARIZE_CHAIN.arun([Document(page_content=text)])
+    return await SUMMARIZE_CHAIN.arun(text)
