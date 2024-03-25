b5f26a2797f9e6df7a06f47f93193ff708ac2758
Punit Arani <punitsai36@gmail.com>
2024-03-25 15:39:13 -0700

    Feature: update summarize_text() to use langchain summarize chain

--- buff/llm/skills/summarize.py
+++ buff/llm/skills/summarize.py
@@ -1,8 +1,9 @@
 """buff/llm/skills/summarize.py"""
 
+from langchain.chains.summarize import load_summarize_chain
+from langchain.schema import Document
+from langchain_openai import ChatOpenAI
 from tenacity import retry, stop_after_attempt, wait_fixed
-
-from buff.llm.client import openai
 
 SUMMARIZE_PAPER_SYSTEM_PROMPT = """
 You will be provided the contents of a research paper.
@@ -15,11 +16,14 @@ Your task is to summarize the meeting notes as follows:
 - Write a list of potential next steps or research directions.
 """.strip()
 
+LLM_OPENAI = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-0125")
+SUMMARIZE_CHAIN = load_summarize_chain(llm=LLM_OPENAI, chain_type="stuff")
+
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
-async def summarize_paper(text: str) -> str:
+async def summarize_text(text: str) -> str:
     """
-    Summarize the paper.
+    Summarize the contents of a research paper.
 
     Args:
         text (str): Contents of the paper
@@ -28,14 +32,4 @@ async def summarize_paper(text: str) -> str:
         str: Summary of the paper
     """
 
-    response = await openai.chat.completions.create(
-        model="gpt-3.5-turbo-0125",
-        messages=[
-            {"role": "system", "content": SUMMARIZE_PAPER_SYSTEM_PROMPT},
-            {"role": "user", "content": text},
-        ],
-        response_format={"type": "text"},
-        temperature=0,
-    )
-
-    return response.choices[0].message.content
+    return await SUMMARIZE_CHAIN.arun([Document(page_content=text)])
